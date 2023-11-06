2d3c30452a67395f70c473733fe242bf00ae857a
Saicharan-LGS <gangula.saicharan@labyrinthglobalsolutions.com>
2023-11-06 17:01:01 +0530

    added routes

--- src/Compoents/Navbar/sidebar.js
+++ src/Compoents/Navbar/sidebar.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
-import AdminHomePage from '../AdminHomePage';
-import DimensionOrderList from '../DimensionOrders';
-import LabelOrders from '../labelOrders';
-import AccountOrders from '../AccountantPage';
+import AdminHomePage from "../AdminHomePage";
+import DimensionOrderList from "../DimensionOrders";
+import LabelOrders from "../labelOrders";
+import AccountOrders from "../AccountantPage";
 
 export const SidebarData = [
   {
-    title: 'Admin',
-    
+    title: "Admin",
+    value: "Admin",
     icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text',
-    component:<AdminHomePage/>
-    
+    cName: "nav-text",
+    component: <AdminHomePage />,
   },
   {
-    title: 'Dimensions',
+    title: "Dimensions",
+    value: "Dimension",
     icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text',
-    component:<DimensionOrderList/>
-    
+    cName: "nav-text",
+    component: <DimensionOrderList />,
   },
   {
-    title: 'Label',
+    title: "Label",
+    value: "Label",
     icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text',
-    component:<LabelOrders/>
+    cName: "nav-text",
+    component: <LabelOrders />,
   },
   {
-    title: 'Accountant',
+    title: "Accountant",
+    value: "accountant",
     icon: <IoIcons.IoMdPeople />,
-    cName: 'nav-text',
-    component:<AccountOrders/>
+    cName: "nav-text",
+    component: <AccountOrders />,
   },
-  {
-    title: 'Customer',
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text'
-  },
-  
+  // {
+  //   title: 'Customer',
+  //   icon: <FaIcons.FaEnvelopeOpenText />,
+  //   cName: 'nav-text'
+  // },
 ];
