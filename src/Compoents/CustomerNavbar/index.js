d59ddd2fb7fcfce31ee601f20a35de6b7133b1e9
Kapilrajreddy <kapilrajreddy@gmail.com>
2023-11-14 17:25:42 +0530

    added notfound item

--- src/Compoents/CustomerNavbar/index.js
+++ src/Compoents/CustomerNavbar/index.js
@@ -1,9 +1,135 @@
+// import React, { useState, useEffect } from "react";
+// import * as FaIcons from "react-icons/fa";
+// import * as AiIcons from "react-icons/ai";
+// import { Link } from "react-router-dom";
+// import { CustomerNavbarData } from "./CustomerNavbar.js";
+// import './index.css';
+// import { IconContext } from "react-icons";
+// import { useNavigate } from "react-router-dom";
+// import CustomerHomePage from "../CustomerHomePage";
+// import CustomerAccepted from "../CustomerHomePage/customerAccepted.js";
+// import CustomerRejected from "../CustomerHomePage/customerRejected.js";
+// import CustomerAllProducts from "../CustomerHomePage/customerAllproducts.js";
+// import CustomerOrder from "../customerOrder/index.js";
+// import TransactionSummary from "./Amount.js";
+
+// function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
+//   const [sidebar, setSidebar] = useState(false);
+//   const [status, setStatus] = useState(() => {
+//     // Get status from cookie or default to 5
+//     let savedStatus = localStorage.getItem("status");
+//     console.log(savedStatus,"SAVED");
+  
+//     // Check if status is null or not a valid number
+//     if (savedStatus === undefined || isNaN(parseInt(savedStatus, 10))) {
+//       // If status is not present in local storage or not a valid number, set it to 5
+//       savedStatus = 5;
+//       localStorage.setItem("status", savedStatus);
+//     } else {
+//       // Parse the valid status to ensure it's a number
+//       savedStatus = parseInt(savedStatus, 10);
+//     }
+  
+//     return savedStatus;
+//   });
+  
+  
+
+//   const [, setCurrentComponent] = useState(
+//     <CustomerHomePage key={status} id={status} />
+//   );
+
+//   console.log(status);
+
+//   const showSidebar = () => setSidebar(!sidebar);
+
+//   const handleSidebarItemClick = (path, component,id) => {
+//     setSidebar(false);
+//     setCurrentComponent(component);
+//     console.log(component,id)
+//     setStatus(id)
+//   };
+
+//   useEffect(() => {
+//     fetchTotalAmount();
+//     handleSidebarItemClick(status);
+//     // Update the currentComponent when status changes
+//     setCurrentComponent(<CustomerHomePage key={status} id={status} />);
+//     // Save status to cookie
+//     localStorage.setItem("status", status);
+//   }, []);
+
+//   const navigate = useNavigate();
+
+//   const handleLogout = () => {
+//     sessionStorage.removeItem("token");
+//     localStorage.removeItem("role");
+//     sessionStorage.removeItem("sname");
+//     navigate("/");
+//   };
+
+//   const name = sessionStorage.getItem("sname");
+
+//   const activeToggle = sidebar ? "menu-bars toggle" : `menu-bars`;
+//   console.log(status)
+//   return (
+//     <div className="navbar-container">
+//       <IconContext.Provider value={{ color: "#000" }}>
+//         <div className={`navbar ${sidebar ? "shifted" : ""}`}>
+//           <Link to="#" className={activeToggle}>
+//             <FaIcons.FaBars onClick={showSidebar} />
+//           </Link>
+//           <div className="navbar-logout-button-container">
+//           <TransactionSummary  totalAmount={totalAmount}   />
+//             <p className="navbar-nav-item-name">{name}</p>
+//             {/* <p className="customer-navbar-nav-item-name">{role}</p> */}
+//             <button className="navbar-logout-button" onClick={handleLogout}>Logout</button>
+//           </div>
+//         </div>
+//         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+//           <ul className="nav-menu-items" onClick={showSidebar}>
+//             <li className="navbar-toggle">
+//               <Link to="#" className="menu-bars">
+//                 <AiIcons.AiOutlineClose />
+//               </Link>
+//               </li>
+//             {CustomerNavbarData.map((item, index) => {
+//               const activeClassName = status===item.id?`active-nav-item nav-text`:`nav-text`
+//               return(
+//               <li key={index} className={activeClassName} onClick={() => handleSidebarItemClick(item.path, item.component,item.id)}>
+//                   {item.icon}
+//                   <span>{item.title}</span>
+//               </li>
+//               );
+//             })}
+//           </ul>
+//         </nav>
+//       </IconContext.Provider>
+//       <div className={`content-container ${sidebar ? "shifted" : ""}`}>
+//         {status === 5 ? (
+//           <CustomerHomePage fetchTotalAmount={fetchTotalAmount} />
+//         ) : status === 6 ? (
+//           <CustomerAccepted />
+//         ) : status === 7 ? (
+//           <CustomerRejected />
+//         ) : status === 8 ? (
+//           <CustomerAllProducts />
+//         ) : (
+//           <CustomerOrder />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CustomerNavbar;
+
 import React, { useState, useEffect } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CustomerNavbarData } from "./CustomerNavbar.js";
-import './index.css';
+import "./index.css";
 import { IconContext } from "react-icons";
 import { useNavigate } from "react-router-dom";
 import CustomerHomePage from "../CustomerHomePage";
@@ -12,16 +138,10 @@ import CustomerRejected from "../CustomerHomePage/customerRejected.js";
 import CustomerAllProducts from "../CustomerHomePage/customerAllproducts.js";
 import CustomerOrder from "../customerOrder/index.js";
 import TransactionSummary from "./Amount.js";
-
-function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
+function CustomerNavbar({ totalAmount,fetchTotalAmount}) {
   const [sidebar, setSidebar] = useState(false);
-  const [status, setStatus] = useState(() => {
-    // Get status from cookie or default to 5
-    const savedStatus = parseInt(localStorage.getItem("status"), 10);
-    return isNaN(savedStatus) ? 5 : savedStatus;
-  });
-
-  const [, setCurrentComponent] = useState(
+  const [status, setStatus] = useState(5);
+  const [currentComponent, setCurrentComponent] = useState(
     <CustomerHomePage key={status} id={status} />
   );
 
@@ -29,22 +149,19 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
 
   const showSidebar = () => setSidebar(!sidebar);
 
-  const handleSidebarItemClick = (path, component,id) => {
-    setSidebar(false);
-    setCurrentComponent(component);
-    setStatus(id)
+  const handleSidebarItemClick = async (id) => {
+    await setSidebar(false);
+    console.log(id, "called");
+    await setStatus(id);
   };
 
   useEffect(() => {
-    fetchTotalAmount();
     handleSidebarItemClick(status);
     // Update the currentComponent when status changes
     setCurrentComponent(<CustomerHomePage key={status} id={status} />);
-    // Save status to cookie
-    localStorage.setItem("status", status);
   }, [status]);
 
-  const navigate = useNavigate();
+  const navigate=useNavigate()
 
   const handleLogout = () => {
     sessionStorage.removeItem("token");
@@ -52,10 +169,13 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
     sessionStorage.removeItem("sname");
     navigate("/");
   };
+  const postOrder=()=>{
+    navigate("/upload")
+  }
+  const role = sessionStorage.getItem("role")
+  const name = sessionStorage.getItem("sname")
 
-  const name = sessionStorage.getItem("sname");
-
-  const activeToggle = sidebar ? "menu-bars toggle" : `menu-bars`;
+  const activeToggle=sidebar?"menu-bars toggle":`menu-bars`
 
   return (
     <div className="navbar-container">
@@ -70,6 +190,7 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
             {/* <p className="customer-navbar-nav-item-name">{role}</p> */}
             <button className="navbar-logout-button" onClick={handleLogout}>Logout</button>
           </div>
+          
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
@@ -77,37 +198,42 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
               <Link to="#" className="menu-bars">
                 <AiIcons.AiOutlineClose />
               </Link>
-              </li>
+            </li>
             {CustomerNavbarData.map((item, index) => {
-              const activeClassName = status===item.id?`active-nav-item nav-text`:`nav-text`
-              
-              return(
-               
-              <li key={index} className={activeClassName} onClick={() => handleSidebarItemClick(item.path, item.component)}>
-                
-                  {item.icon}
-                  <span>{item.title}</span>
+              console.log(item.id);
+              const activeClassName = status===item.id?`active-nav-item nav-text a`:`nav-text`
+              return (
+                <li
+                  key={index}
+                  className={activeClassName}
+                  onClick={() => handleSidebarItemClick(item.id)}
+                >
                   
-              </li>
+                    {item.icon}
+                    <span className=".span">{item.title}</span>
+                </li>
               );
             })}
+            
           </ul>
+          
         </nav>
       </IconContext.Provider>
 
       <div className={`content-container ${sidebar ? "shifted" : ""}`}>
         {status === 5 ? (
           <CustomerHomePage fetchTotalAmount={fetchTotalAmount} />
-        ) : status === 6 ? (
+        ) : status ===6 ? (
           <CustomerAccepted />
         ) : status === 7 ? (
           <CustomerRejected />
         ) : status === 8 ? (
           <CustomerAllProducts />
         ) : (
-          <CustomerOrder />
+          <CustomerOrder/>
         )}
       </div>
+    
     </div>
   );
 }
--- src/Compoents/StaffLogin/index.js
+++ src/Compoents/StaffLogin/index.js
@@ -1,109 +1,18 @@
-// import React, { useState } from "react";
-// import "./index.css";
 
-// const StaffSigninPage = () => {
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: "",
-//   });
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     // Define the URL of your server's login endpoint
-//     const url = "http://localhost:3009/api/v1/stafflogin";
-
-//     // Create a JSON object with the form data
-//     const jsonData = {
-//       email: formData.email,
-//       password: formData.password,
-//     };
-
-//     // Make a POST request using the fetch API with JSON data
-//     fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json", // Set the Content-Type to JSON
-//       },
-//       body: JSON.stringify(jsonData), // Convert JSON object to string
-//     })
-//       .then((response) => {
-//         if (response.status === 200) {
-//           // Login successful
-//           response.json().then((data) => {
-//             // Store the token in sessionStorage
-//             console.log(data.token, data.role)
-//             sessionStorage.setItem("token", data.token);
-//             sessionStorage.setItem("role",data.role)
-//             console.log("Login successful");
-//           });
-//         } else {
-//           // Handle other status codes or error messages
-//           console.error("Login failed");
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error:", error);
-//       });
-//   };
-
-//   return (
-//     <div className="signin-div-container">
-//     <div className="signin-form-main-container">
-//       <center>
-//         <h2 className="signin-form-heading-container">Sign In</h2>
-//       </center>
-//       <form onSubmit={handleSubmit} className="signin-form-container">
-//         <div className="signin-form-group-container">
-//           <label className="signin-form-label-container">Email:</label>
-//           <input
-//             type="email"
-//             name="email"
-//             className="signin-input-text"
-//             value={formData.email}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div className="signin-form-group-container">
-//           <label className="signin-form-label-container">Password:</label>
-//           <input
-//             type="password"
-//             name="password"
-//             className="signin-input-text"
-//             value={formData.password}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <center>
-//           <button className="signin-form-button-container" type="submit">
-//             Sign In
-//           </button>
-//         </center>
-//       </form>
-//     </div>
-//     </div>
-//   );
-// };
-
-// export default StaffSigninPage;
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import React Router
 import "./index.css";
 import Toast from "../utlis/toast";
+import CustomerLogin from "../CustomerLogin";
 
 const StaffSigninPage = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
+  const [staff,setStaff] = useState(true)
 
   const [error, setError] = useState(""); // State variable for error message
   const navigate = useNavigate(); // Get access to the navigation history
@@ -183,23 +92,31 @@ const StaffSigninPage = () => {
   };
 
   const onClickCustomer = () => {
-    navigate("/CustomerLogin");
+    setStaff(false)
   };
+  const onClickStaff=()=>{
+    setStaff(true)
+  }
+
+  const activeStaffButton = staff ? `signin-staff-button`:`signin-customer-button`
+  const activeCustomerButton = staff?`signin-customer-button`:`signin-staff-button`
 
   return (
+    <>
     <div className="signin-div-container">
       <div className="login-image-container">
         <img src="https://www.ascarii.com/hubfs/Optimised-Customer-Service-v4.png" className="Login-image" alt=""/>
       </div>
       <div className="signin-form-main-container">
         <div className="signin-staff-customer-button-container">
-          <button className="signin-staff-button">Staff Signin</button>
-          <button className="signin-customer-button" onClick={onClickCustomer}>
+          <button className={activeStaffButton} onClick={onClickStaff}>Staff Signin</button>
+          <button className={activeCustomerButton} onClick={onClickCustomer}>
             Customer Signin
           </button>
         </div>
-
-        <center>
+      {staff ?(    
+        <>
+          <center>
           <h2 className="signin-form-heading-container">Staff Login</h2>
         </center>
 
@@ -234,8 +151,11 @@ const StaffSigninPage = () => {
             {/* Display error message */}
           </center>
         </form>
+        </>
+      ): <CustomerLogin />}
       </div>
     </div>
+    </>
   );
 };
 
