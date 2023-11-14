4394d3096f8a910184edfc918441519512fd3b52
Prashanth Reddy <a.prashanth5reddy@gmail.com>
2023-11-14 12:07:54 +0530

    abcd

--- src/Compoents/CustomerNavbar/index.js
+++ src/Compoents/CustomerNavbar/index.js
@@ -12,9 +12,15 @@ import CustomerRejected from "../CustomerHomePage/customerRejected.js";
 import CustomerAllProducts from "../CustomerHomePage/customerAllproducts.js";
 import CustomerOrder from "../customerOrder/index.js";
 import TransactionSummary from "./Amount.js";
+
 function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
   const [sidebar, setSidebar] = useState(false);
-  const [status, setStatus] = useState(5);
+  const [status, setStatus] = useState(() => {
+    // Get status from cookie or default to 5
+    const savedStatus = parseInt(localStorage.getItem("status"), 10);
+    return isNaN(savedStatus) ? 5 : savedStatus;
+  });
+
   const [, setCurrentComponent] = useState(
     <CustomerHomePage key={status} id={status} />
   );
@@ -30,9 +36,12 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
   };
 
   useEffect(() => {
+    fetchTotalAmount();
     handleSidebarItemClick(status);
     // Update the currentComponent when status changes
     setCurrentComponent(<CustomerHomePage key={status} id={status} />);
+    // Save status to cookie
+    localStorage.setItem("status", status);
   }, [status]);
 
   const navigate = useNavigate();
@@ -43,10 +52,7 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
     sessionStorage.removeItem("sname");
     navigate("/");
   };
-  // const postOrder=()=>{
-  //   navigate("/upload")
-  // }
-  // const role = sessionStorage.getItem("role")
+
   const name = sessionStorage.getItem("sname");
 
   const activeToggle = sidebar ? "menu-bars toggle" : `menu-bars`;
@@ -61,7 +67,6 @@ function CustomerNavbar({ totalAmount, fetchTotalAmount }) {
           <div className="customer-navbar-logout-button-container">
             <TransactionSummary totalAmount={totalAmount} />
             <p className="customer-navbar-nav-item-name">{name}</p>
-            {/* <p className="customer-navbar-nav-item-name">{role}</p> */}
             <button className="navbar-logout-button" onClick={handleLogout}>
               Logout
             </button>
